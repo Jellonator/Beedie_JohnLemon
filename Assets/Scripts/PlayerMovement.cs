db62924ffc1bf2c5bf3314ada9d37fd32f8235f1
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-10 21:58:40 -0400

    Set up for movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /// Turning speed
+    public float turnSpeed = 20.0f;
     /// Movement vector
     Vector3 m_Movement = Vector3.zero;
     /// Animator component
     Animator m_Animator;
-    /// Turning speed
-    public float turnSpeed = 20.0f;
     /// Player's rotation
     Quaternion m_Rotation = Quaternion.identity;
+    /// Player's Rigidbody component
+    Rigidbody m_Rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -36,5 +39,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         // Set player's rotation
         m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        
     }
 }
