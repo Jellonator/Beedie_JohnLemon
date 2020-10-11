db4d77d6749a0d682f539154ee691be831e8fdb3
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-11 01:29:17 -0400

    Added bool to check if player exited

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -8,13 +8,24 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;
     /// Reference to the player
     public GameObject player;
+    /// True if the player has exited
+    bool m_IsPlayerAtExit = false;
 
     /// Called when a Collider enters this object's trigger
     void OnTriggerEnter(Collider other)
     {
+        // check that collider is the player object
         if (other.gameObject == player)
         {
+            m_IsPlayerAtExit = true;
+        }
+    }
 
+    /// Called every frame
+    void Update()
+    {
+        if (m_IsPlayerAtExit) {
+            
         }
     }
 }
