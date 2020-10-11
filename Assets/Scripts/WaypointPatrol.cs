27b52b41a73776868217b4b41dc82a2e6eda2cb1
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-11 04:38:41 -0400

    Added waypoint array to waypointpatrol

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
+    /// Reference to the NavMeshAgent
+    public NavMeshAgent navMeshAgent;
+    /// A list of Transforms to use as waypoints.
+    public Transform[] waypoints;
+
     void Start()
     {
-        
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     void Update()
