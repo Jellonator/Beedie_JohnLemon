af87340f0390fc5ce1ce9e5c1ed0847aebb12df2
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-10 21:59:45 -0400

    Implemented movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -43,6 +43,6 @@ public class PlayerMovement : MonoBehaviour
 
     void OnAnimatorMove()
     {
-        
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
     }
 }
