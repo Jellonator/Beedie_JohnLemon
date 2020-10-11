22ee55fba431e83bcbe5a1ba2adb2bdc214746cc
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-11 05:11:04 -0400

    Cleaned up waypointpatrol

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -14,8 +14,10 @@ public class WaypointPatrol : MonoBehaviour
 
     void Start()
     {
+        // get the nav mesh agent
         navMeshAgent = GetComponent<NavMeshAgent>();
         if (waypoints.Length > 0) {
+            // if there's at least 1 waypoint, target it first
             navMeshAgent.SetDestination(waypoints[0].position);
         }
     }
@@ -23,7 +25,10 @@ public class WaypointPatrol : MonoBehaviour
     void FixedUpdate()
     {
         if (waypoints.Length > 1) {
+            // if there's at least two waypoints
             if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance) {
+                // if the enemy is close to its target location,
+                // go to the next waypoint
                 m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
                 navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
             }
