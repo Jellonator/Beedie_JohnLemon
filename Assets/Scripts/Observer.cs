ac59fb5f2284d5f385ea80fbe6d34090e6734c71
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-11 02:01:03 -0400

    Observer now detects player

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Observer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// Reference to the player's transform
+    public Transform player;
+    /// True if the player has been detected
+    bool m_IsPlayerInRange = false;
+
+    /// Called when collider enters trigger
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.transform == player) {
+            m_IsPlayerInRange = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// Called when a collider leaves the trigger
+    void OnTriggerExit(Collider other) {
+        if (other.transform == player) {
+            m_IsPlayerInRange = false;
+        }
     }
 }
