cc519b6c40d52ff67e0695ef2391e77767dd6aff
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-18 04:36:18 -0400

    Changed camera control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        if (movement.sqrMagnitude > 1f) {
+            movement.Normalize();
+        }
+        transform.position += movement * Time.deltaTime * cameraSpeed;
+    }
+}
