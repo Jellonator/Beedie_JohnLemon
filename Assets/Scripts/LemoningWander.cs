dc409a78cbe516c4b4cbcad665b16ef43f151700
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-20 00:39:26 -0400

    Commented lemoning wandering script

--- Assets/Scripts/LemoningWander.cs
+++ Assets/Scripts/LemoningWander.cs
@@ -5,20 +5,21 @@ using UnityEngine.AI;
 
 public class LemoningWander : MonoBehaviour
 {
+    /// The minimum amount of distance that a position must be for a lemoning to wander to it.
     public float minimumMoveDistance = 0.5f;
-
+    /// The maximum amount of distance that a Lemoning will attempt to wander to.
     public float maximumMoveDistance = 2.5f;
-
+    /// The number of positions that a lemoning will attempt to move to.
     public int moveAttempts = 3;
-
+    /// The minimum amount of time that a lemoning will spend waiting to wander.
     public float minimumTimer = 1.0f;
-
+    /// The maximum amount of time that a lemoning will spend waiting to wander.
     public float maximumTimer = 5.0f;
-
+    /// Reference to this character's LemoningController
     private LemoningController m_Controller;
-
+    /// Tracks how long this lemoning has been waiting for
     private float m_waitTimer = 0.0f;
-
+    /// How long a lemoning needs to wait before wandering
     private float m_targetWait = 1.0f;
 
     // Start is called before the first frame update
@@ -29,11 +30,13 @@ public class LemoningWander : MonoBehaviour
         m_waitTimer = 0.0f;
     }
 
+    /// Randomizes the timer to a new value in range [minimumTimer, maximumTimer]
     private void RandomizeTimer()
     {
         m_targetWait = Random.Range(minimumTimer, maximumTimer);
     }
 
+    /// Attempt to move the lemoning to a new location
     private void AttemptMove()
     {
         for (int i = 0; i < moveAttempts; i++) {
