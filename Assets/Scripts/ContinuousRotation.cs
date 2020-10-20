3609ec0c2c131f25db55faa0caa0087fb898abff
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-20 11:31:57 -0400

    Created spinning obstacle

---
+++ Assets/Scripts/ContinuousRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousRotation : MonoBehaviour
+{
+    /// Axis around which this spinner rotates
+    public Vector3 axis = Vector3.up;
+    /// Speed at which this spinner rotates
+    public float speed = 45.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.RotateAround(transform.position, axis, speed * Time.deltaTime);
+    }
+}
