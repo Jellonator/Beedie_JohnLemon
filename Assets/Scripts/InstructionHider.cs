4b8f9ae45e5199b1d0b8cfbbbaf92ad833d74267
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-26 18:52:25 -0400

    Instruction text now hides when player moves around a bit

---
+++ Assets/Scripts/InstructionHider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionHider : MonoBehaviour
+{
+    public CanvasGroup instructions;
+
+    /// Called when a Collider enters this object's trigger
+    void OnTriggerEnter(Collider other)
+    {
+        // check that collider is the player object, and that the state is 'playing'
+        if (other.tag == "Lemoning") {
+            instructions.alpha = 0.0f;
+        }
+    }
+}
