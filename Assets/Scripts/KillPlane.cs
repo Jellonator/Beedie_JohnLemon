3183b303c9b6bb5440d6ba90e74bfe0a3babee4b
Jocelyn Beedie <jellonator00@gmail.com>
2020-10-26 20:17:15 -0400

    Game now fails if all lemonnings drop

---
+++ Assets/Scripts/KillPlane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour
+{
+    public GameEnding gameEnding;
+
+    /// Called when a Collider enters this object's trigger
+    void OnTriggerEnter(Collider other)
+    {
+        // check that collider is a lemoning and has not yet been found
+        if (other.tag == "Lemoning") {
+            gameEnding.DropPlayer();
+            other.gameObject.SetActive(false);
+        }
+    }
+}
